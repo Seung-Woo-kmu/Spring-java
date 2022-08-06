8c334f81659f25be1c7e93507194cd1901bc105c
유승우 <66248758+Seung-Woo-kmu@users.noreply.github.com>
2022-08-06 18:46:12 +0900

    revise to follow DIP
    revise to follow DIP

--- core/src/main/java/hello/core/member/MemberRepository.java
+++ core/src/main/java/hello/core/member/MemberRepository.java
@@ -4,4 +4,11 @@ public interface MemberRepository {
     void save(Member member);
 
     Member findById(Long memberId);
+
+    default int happy() {
+        return 1;
+    }
+    static void happier() {
+        System.out.println("true = " + true);
+    }
 }
--- core/src/test/java/hello/core/member/MemberServiceTest.java
+++ core/src/test/java/hello/core/member/MemberServiceTest.java
@@ -1,11 +1,22 @@
 package hello.core.member;
 
+import hello.core.AppConfig;
+import hello.core.order.OrderService;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 
 public class MemberServiceTest {
+    MemberService memberService;
+    OrderService orderService;
 
-    MemberService memberService = new MemberServiceImpl();
+    @BeforeEach
+    public void beforeEach() {
+        AppConfig appConfig = new AppConfig();
+        memberService = appConfig.memberService();
+    }
+
     @Test
     void join(){
         Member member = new Member(1l, "memberA",Grade.VIP);
