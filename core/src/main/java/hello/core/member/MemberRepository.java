58187be31ad1080ea2db14d520b0a6448779ac69
유승우 <66248758+Seung-Woo-kmu@users.noreply.github.com>
2022-08-11 20:34:29 +0900

    Use Spring
    Use Spring

--- core/src/main/java/hello/core/member/MemberRepository.java
+++ core/src/main/java/hello/core/member/MemberRepository.java
@@ -5,10 +5,4 @@ public interface MemberRepository {
 
     Member findById(Long memberId);
 
-    default int happy() {
-        return 1;
-    }
-    static void happier() {
-        System.out.println("true = " + true);
-    }
 }
